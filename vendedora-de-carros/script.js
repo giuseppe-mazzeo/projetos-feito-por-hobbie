73c2d3716d35dc5e324d1262c158c676f696e9dc
giuseppe-mazzeo-a22204542 <giumazz03@gmail.com>
2024-04-02 12:32:56 +0100

    Initial commit

---
+++ vendedora-de-carros/script.js
@@ -0,0 +1,8 @@
+/* 
+document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(function() {
+        document.querySelector('#srPeppino').classList.add('oculta');
+    }, 500);
+});
+
+*/
